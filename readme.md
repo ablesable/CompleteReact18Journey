46098eddcc9c94ab09655453a40cc68470bd3ab7
ablesable <sobolbartosz@gmail.com>
2023-05-19 10:37:41 +0200

    Using effects with conditional parameters

--- readme.md
+++ readme.md
@@ -1,28 +1,33 @@
 #### Learning new React from scratch with Brian Holt course on Frontend Masters.
 
 ## Intro
-- two scripts in the index file (react and react-dom), makes them behave as a global variable. 
+
+- two scripts in the index file (react and react-dom), makes them behave as a global variable.
 - React is an API part of a library, and react-dom is actual renderer on a DOM.
 - App function needs to be called. That's why we add renderer in script tags.
 - JSX is translated into the JS. Then React.createElement is called.
 - Components always has to return markup, the result of React.createElement.
 - for developer dependencies use npm install --save-dev
 - for formatting I use prettier. To use it for specific project, go to the settings: prettier -> require config file, prettier needs to be default formatter. That way, prettier will run only in projects with spcific instructions.
-- add this to scripts: "format": "prettier --write \"src/**/*.{js,jsx}\"", then npm run format will format all the code.
+- add this to scripts: "format": "prettier --write \"src/\*_/_.{js,jsx}\"", then npm run format will format all the code.
 - for eslint install additional extension for vscode, just like with prettier. Before script format is for people which doesn't use vscode. For VSCode previous settings needs to be added + format on save setting.
-- add this to scripts: "lint": "eslint \"src/**/*.{js, jsx}\" --quiet"
+- add this to scripts: "lint": "eslint \"src/\*_/_.{js, jsx}\" --quiet"
 - For specific debugging: npm run lint -- --debug (first -- is not passing anything to npm, second -- is for passing to eslint).
 - type="module" in index.html is for vite. We don't use typical JavaScript but ES6 modules.
 - For Eslint to understand jsx/tsx, We need to install plugins: import, react, jsx-a11y.
 
 ## JavaScript rules in React
+
 - () parantheses in return are telling JS that line is not ended after return keyword, but after closing paranthesis.
 - className keyword is used, cause class in JS is a keyword reserved to... classes.
 
 ## Hooks
+
 - Every event in JavaScript, when happens, it rerenders everything.
 - Because of rerendering cycle, we have to be more explicit about changing value (Hook).
 - Hook - useState() returns array of two values. First is current state, and second is a function to change that state.
 - Hooks needs to be called in the same order, when we're referencing the same component. That's how react keep track of things.
 - RENDER FUNCTION needs to be fast and stateless!
 - Custom Hooks are bundled from a few other hooks that exists.
+- Effect Hook is a way to get something from out of component (Typical API request).
+- Second argument in useEffect means that this hook will be called not every time, but just then, when value under this second argument changes.
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,67 +1,79 @@
-import { useState } from 'react';
-const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-const breeds = [];
+import { useState, useEffect } from 'react';
+import Pet from './Pet';
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA"); //const
-    //can be used here, cause the whole SearchParams function needs to be 
-    //called again to reassign value there.
+  const breeds = [];
 
-    const [animal, setAnimal] = useState("");
-    const [breed, setBreed] = useState("");
+  const [location, setLocation] = useState('Seattle, WA'); //const can be used here, cause the whole SearchParams function needs to be called again to reassign value there.
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const [pets, setPets] = useState([]);
 
-    return (
-       <div className="search-params">
-        <form>
-            <label htmlFor="location">
-                Location
-                <input onChange={(e) => setLocation(e.target.value)}
-                id="location" 
-                value={location} 
-                placeholder="Location" />
-            </label>
-            <label htmlFor="animal">
-                Animal
-                <select
-                    id="animal"
-                    value={animal}
-                    onChange={e=> {
-                        setAnimal(e.target.value);
-                        setBreed("");
-                    }}
-                    
-                >
+  useEffect(() => {
+    requestPets();
+  }, [location]);
 
-                    {ANIMALS.map((animal) => ( //parentheses used cause of implicit return
-                        <option key={animal}>{animal}</option>
-                    ))}
-                    
-                </select>  
-            </label>
+  async function requestPets() {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+    const json = await res.json();
 
-            <label htmlFor="breed">
-                Breed
-                <select
-                    id="breed"
-                    value={breed}
-                    disabled={breeds.length===0}
-                    onChange={e=> {
-                        setBreed(e.target.value)
-                    }}>
+    setPets(json.pets);
+  }
 
-                    {breeds.map((breed) => ( //parentheses used cause of implicit return
-                        <option key={breed}>{breed}</option>
-                    ))}
-                    
-                </select>  
-            </label>
+  return (
+    <div className='search-params'>
+      <form>
+        <label htmlFor='location'>
+          Location
+          <input onChange={(e) => setLocation(e.target.value)} id='location' value={location} placeholder='Location' />
+        </label>
+        <label htmlFor='animal'>
+          Animal
+          <select
+            id='animal'
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed('');
+            }}
+          >
+            {ANIMALS.map(
+              (
+                animal, //parentheses used cause of implicit return
+              ) => (
+                <option key={animal}>{animal}</option>
+              ),
+            )}
+          </select>
+        </label>
 
+        <label htmlFor='breed'>
+          Breed
+          <select
+            id='breed'
+            value={breed}
+            disabled={breeds.length === 0}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            {breeds.map(
+              (
+                breed, //parentheses used cause of implicit return
+              ) => (
+                <option key={breed}>{breed}</option>
+              ),
+            )}
+          </select>
+        </label>
 
-            <button>Submit</button>
-        </form>
-       </div> 
-    )
-
-
-}
+        <button>Submit</button>
+      </form>
+      {pets.map((pet) => (
+        <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+      ))}
+    </div>
+  );
+};
 export default SearchParams;
