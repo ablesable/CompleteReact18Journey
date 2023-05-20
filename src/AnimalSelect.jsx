8904a761927689f334778f48c4e8d5f0043de0e3
ablesable <sobolbartosz@gmail.com>
2023-05-20 20:49:13 +0200

    making smaller components

---
+++ src/AnimalSelect.jsx
@@ -0,0 +1,19 @@
+const AnimalSelect = (props) => {
+  return (
+    <label htmlFor='animal'>
+      Animal
+      <select
+        id='animal'
+        value={props.animal}
+        onChange={(e) => {
+          props.setAnimal(e.target.value);
+          props.setBreed('');
+        }}
+      >
+        {props.ANIMALS.map((animal) => (
+          <option key={animal}>{animal}</option>
+        ))}
+      </select>
+    </label>
+  );
+};
